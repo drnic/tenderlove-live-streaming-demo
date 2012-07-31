ad81271cc09051add880c56e37f332a472ac656c
Dr Nic Williams <drnicwilliams@gmail.com>
2012-07-30 20:18:45 -0700

    initial rails4-edge scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# Make sure your secret_token is kept private
+# if you're sharing your code publicly.
+TenderloveLiveStreamingDemo::Application.config.secret_token = '9ac4857f2a9fdd37c9690ce72c3a2ab261ed620e0394e04a56a03231c33850d4794629a3d5c5fbc689dedd4e70f35728471d819766f86f88323d53850e43a060'
